bbc5cfc6710d765aa9fc8a6adbc80e0a18ac0501
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-02-10 22:51:57 +0100

    proper rpms

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM centos:latest
 MAINTAINER "Didrik A. Rognstad" <didrik.rognstad@gmail.com>
 # Install dependencies
-RUN yum -y install git libtool make automake gcc gcc-c++ byacc flex rpm-build openssl-devel libxml2-devel boost-devel hwloc-devel which
+RUN yum -y install openssh-clients wget libtool make automake gcc gcc-c++ byacc flex rpm-build openssl-devel libxml2-devel boost-devel hwloc-devel which
 
 # Create folders
 RUN mkdir /compile
@@ -10,5 +10,3 @@ RUN mkdir /artifacts
 # Copy in build.sh
 COPY build.sh /
 ENTRYPOINT /build.sh
-
-
--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-cd /compile && git clone https://github.com/dizk/torque.git -b 6.0.0.1 torque
-cd /compile/torque && ./buildutils/torque-build -U
-cp /compile/torque/buildutils/rpmbuild/RPMS/x86_64/* /artifacts
+VERSION="torque-6.0.0.1"
+FILENAME="$VERSION.tar.gz"
+wget http://wpfilebase.s3.amazonaws.com/torque/torque-6.0.0.1-1449528029_21cc3d8.tar.gz -qO $FILENAME
+
+# Repack - because...
+tar xvzf $FILENAME
+rm $FILENAME
+mv torque-6.0.0.1* $VERSION
+tar zcvf $FILENAME $VERSION
+
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp $VERSION/torque.spec ~/rpmbuild/SPECS/
+cp $FILENAME ~/rpmbuild/SOURCES/
+
+rpmbuild -bs --target x86_64 --nodeps ~/rpmbuild/SPECS/torque.spec
+rpmbuild -bb --target x86_64 --nodeps ~/rpmbuild/SPECS/torque.spec
+
+
+cp ~/rpmbuild/RPMS/x86_64/* /artifacts
